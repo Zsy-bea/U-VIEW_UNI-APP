3ddf9e4f4fe0ff7b6b6ff8d061cded37bde42bdb
waillyer <1032060699@qq.com>
2021-02-25 14:03:57 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,64 @@
+<template>
+	<view class="content">
+		<u-grid :col="3">
+			<u-grid-item>
+				<u-icon name="photo" :size="46"></u-icon>
+				<view class="grid-text">图片</view>
+			</u-grid-item>
+			<u-grid-item>
+				<u-icon name="lock" :size="46"></u-icon>
+				<view class="grid-text">锁头</view>
+			</u-grid-item>
+			<u-grid-item>
+				<u-icon name="hourglass" :size="46"></u-icon>
+				<view class="grid-text">沙漏</view>
+			</u-grid-item>
+			<u-grid-item>
+				<u-badge count="9" :offset="[20, 20]"></u-badge>
+				<u-icon name="photo" :size="46"></u-icon>
+				<view class="grid-text">图片</view>
+			</u-grid-item>
+			<u-grid-item>
+				<image src="/static/image/icon/hot5.png" class="badge-icon"></image>
+				<u-icon name="lock" :size="46"></u-icon>
+				<view class="grid-text">锁头</view>
+			</u-grid-item>
+			<u-grid-item>
+				<u-icon name="hourglass" :size="46"></u-icon>
+				<view class="grid-text">沙漏</view>
+			</u-grid-item>
+		</u-grid>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style scoped lang="scss">
+	.badge-icon {
+		position: absolute;
+		top: 14rpx;
+		right: 40rpx;
+		width: 30rpx;
+		height: 30rpx;
+	}
+
+	.grid-text {
+		font-size: 28rpx;
+		margin-top: 4rpx;
+		color: $u-type-info;
+	}
+</style>
